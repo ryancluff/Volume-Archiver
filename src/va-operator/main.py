8e7f786ebe61d0126f1fa1327b1f29df5e607a99
Ryan Cluff <ryancluf@yahoo.com>
2024-05-22 02:02:43 +0000

    commit changes on dev

--- src/va-operator/main.py
+++ src/va-operator/main.py
@@ -34,13 +34,12 @@ async def startup_fn(logger, **kwargs):
 
 @kopf.on.create('volumearchiver.rcluff.com', 'v1', 'backupjob')
 def backupjob_created(spec, name, namespace, logger, **kwargs):
-    result = {
-        'replicas': {},
-    }
 
     core_api = kubernetes.client.CoreV1Api()
     apps_api = kubernetes.client.AppsV1Api()
     batch_api = kubernetes.client.BatchV1Api()
+
+    change_state(name, namespace, "job created")
 
     pvc_name = spec.get('pvc')
 
@@ -52,27 +51,27 @@ def backupjob_created(spec, name, namespace, logger, **kwargs):
     if pvc is None:
         raise kopf.PermanentError(f"PVC does not exist")
 
-    if 'ReadWriteOnce' in pvc.spec.access_modes:
-        deployments = apps_api.list_namespaced_deployment(namespace=namespace)
+    # if 'ReadWriteOnce' in pvc.spec.access_modes:
+    #     deployments = apps_api.list_namespaced_deployment(namespace=namespace)
 
-        for deployment in deployments.items:
-            for volume in deployment.spec.template.spec.volumes:
-                if volume.name == pvc_name:
-                    result['replicas'][deployment.metadata.name] = deployment.spec.replicas
+    #     for deployment in deployments.items:
+    #         for volume in deployment.spec.template.spec.volumes:
+    #             if volume.name == pvc_name:
+    #                 result['replicas'][deployment.metadata.name] = deployment.spec.replicas
 
-        replicas_0 = {
-            "spec": {
-                "replicas": 0
-            }
-        }
+    #     replicas_0 = {
+    #         "spec": {
+    #             "replicas": 0
+    #         }
+    #     }
 
-        for deployment in result['replicas']:
-            response = apps_api.patch_namespaced_deployment(
-                name=deployment,
-                namespace=namespace,
-                body=replicas_0
-            )
-            # logger.info(response)
+    #     for deployment in result['replicas']:
+    #         response = apps_api.patch_namespaced_deployment(
+    #             name=deployment,
+    #             namespace=namespace,
+    #             body=replicas_0
+    #         )
+    #         # logger.info(response)
 
     change_state(name, namespace, "creating job")
     path = os.path.join(os.path.dirname(__file__),
@@ -103,41 +102,39 @@ def backupjob_created(spec, name, namespace, logger, **kwargs):
             body=data
         )
 
-    change_state(name, namespace, "backup running")
-
-    return result
+    change_state(name, namespace, "job created")
 
 
-@kopf.on.update('batch', 'v1', 'jobs', field='status.succeeded', value=1, annotations={'volumearchiver.rcluff.com/backupjob': kopf.PRESENT})
-def job_succeeded(name, namespace, meta, **_):
-    apps_api = kubernetes.client.AppsV1Api()
-    cobject_api = kubernetes.client.CustomObjectsApi()
+# @kopf.on.update('batch', 'v1', 'jobs', field='status.succeeded', value=1, annotations={'volumearchiver.rcluff.com/backupjob': kopf.PRESENT})
+# def job_succeeded(name, namespace, meta, **_):
+#     apps_api = kubernetes.client.AppsV1Api()
+#     cobject_api = kubernetes.client.CustomObjectsApi()
 
-    backupjob_name = meta['annotations']['volumearchiver.rcluff.com/backupjob']
+#     backupjob_name = meta['annotations']['volumearchiver.rcluff.com/backupjob']
 
-    backupjob = cobject_api.get_namespaced_custom_object(
-        group='volumearchiver.rcluff.com',
-        version='v1',
-        namespace=namespace,
-        plural='backupjobs',
-        name=backupjob_name
-    )
+#     backupjob = cobject_api.get_namespaced_custom_object(
+#         group='volumearchiver.rcluff.com',
+#         version='v1',
+#         namespace=namespace,
+#         plural='backupjobs',
+#         name=backupjob_name
+#     )
 
-    for deployment in backupjob['status']['backupjob_created']['replicas']:
-        replicas = backupjob['status']['backupjob_created']['replicas'][deployment]
-        response = apps_api.patch_namespaced_deployment(
-            name=deployment,
-            namespace=namespace,
-            body={
-                "spec": {
-                    "replicas": replicas
-                }
-            }
-        )
+#     for deployment in backupjob['status']['backupjob_created']['replicas']:
+#         replicas = backupjob['status']['backupjob_created']['replicas'][deployment]
+#         response = apps_api.patch_namespaced_deployment(
+#             name=deployment,
+#             namespace=namespace,
+#             body={
+#                 "spec": {
+#                     "replicas": replicas
+#                 }
+#             }
+#         )
 
-    change_state(name, namespace, "complete")
+#     change_state(name, namespace, "complete")
 
-@kopf.on.update('batch', 'v1', 'jobs', field='status.succeeded', value=1, annotations={'volumearchiver.rcluff.com/backupjob': kopf.PRESENT})
+@kopf.on.event('batch', 'v1', 'jobs', field='status.succeeded', value=1, annotations={'volumearchiver.rcluff.com/backupjob': kopf.PRESENT})
 def job_succeeded(name, namespace, meta, **_):
     apps_api = kubernetes.client.AppsV1Api()
     cobject_api = kubernetes.client.CustomObjectsApi()
